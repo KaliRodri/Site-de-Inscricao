9ac1dd228a6337b55aaa3f94c50fbcc9101fdb05
Law <kazemarus@outlook.com>
2025-08-29 10:58:17 -0300

    Inscricao ja recebe os dados , validar e enviar dados ao banco de dados

--- src/controllers/inscricoesController.js
+++ src/controllers/inscricoesController.js
@@ -2,14 +2,20 @@ const {Inscricao, Evento} = require('../models');
 class InscricaoController {
     async store(req, res) {
         try {
-            const { evento_id, nome, email, curso } = req.body;
+            const { id: evento_id} = req.params;// pegar id da url de evento
+            const { nome, email, curso } = req.body;
             //validaçao
-            if (!evento_id || !nome || !email || !curso) {
+            if (!nome || !email) {
                 return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
             }
 
             //verifica se o evento existe
-            const evento = await Evento.findByPk(evento_id);
+            const evento = await Evento.findOne({
+                where: {
+                    id: evento_id,
+                    status: 'active'
+                }
+            });
             if (!evento) {
                 return res.status(404).json({ error: 'Evento não encontrado.' });
             }
@@ -21,9 +27,12 @@ class InscricaoController {
                 curso,
             });
             //retorna a inscrição criada
-            return res.status(201).json(novaInscricao);
+            return res.status(201).json({
+                message: 'Inscrição realizada com sucesso!',
+                inscricao: novaInscricao
+            });
         } catch (error) {
-            if (error.name === 'SequelizeValidationError') {
+            if (error.name === 'SequelizeUniqueConstraintError') {
                 return res.status(409).json({ error: 'Este email já está inscrito neste evento.'});
             }
             console.error('Erro ao criar inscrição:', error);
